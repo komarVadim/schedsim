c43bfee8368539a0a42c007c952b1a003551202b
komarVadim <komarVadim@gmail.com>
2016-02-21 11:45:02 +0300

    delete sudo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y python3-numpy
-sudo apt-get install -y python3-matplotlib
-sudo apt-get install -y python3-blist
+apt-get install -y python3-numpy
+apt-get install -y python3-matplotlib
+apt-get install -y python3-blist
 ./experiment_weibull.py 0.25 test
