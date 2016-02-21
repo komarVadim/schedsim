e9ec089f894fdd989a2d71a8f1c84ddebdc5ebde
komarVadim <komarVadim@gmail.com>
2016-02-21 12:13:09 +0300

    fix

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 sudo apt-get install -y python3-numpy
 sudo apt-get install -y python3-matplotlib
 sudo apt-get install -y python3-blist
-./experiment_weibull.py 0.25 test
+python experiment_weibull.py 0.25 test
